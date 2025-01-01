5c4cc0c0a3254917b89db66049c26008efa76d8e
Jorge O. Castro <jorge.castro@gmail.com>
2025-01-01 18:17:47 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,8 +13,15 @@ ln -sr /etc/containers/systemd/*.container /usr/lib/bootc/bound-images.d/
 
 dnf install -y cockpit cockpit-machines cockpit-podman cockpit-files
 
+# Tailscale
+dnf config-manager --add-repo https://pkgs.tailscale.com/stable/centos/9/tailscale.repo
+dnf config-manager --set-disabled tailscale-stable
+dnf -y --enablerepo tailscale-stable install \
+  tailscale
+
 # Services
 
 systemctl enable podman.socket
 systemctl enable cockpit.socket
+systemctl enable tailscaled.service
 systemctl disable auditd.service
