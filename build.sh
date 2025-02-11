ebfc0a8ccb873c6c233e2760885dba8b1c07c95d
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-02-11 17:46:24 +0100

    feat: Add usbutils

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ set -ouex pipefail
 
 dnf install -y avahi cockpit cockpit-machines cockpit-podman cockpit-files libvirt tmux vim firewalld
 
+dnf install -y \
+    usbutils
+
 # Docker install: https://docs.docker.com/engine/install/centos/#install-using-the-repository
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install -y docker-ce docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -25,6 +28,7 @@ systemctl enable cockpit.socket
 systemctl enable tailscaled.service
 systemctl enable docker.service
 systemctl enable docker.socket
+systemctl disable rpm-ostree-countme.timer
 
 # Don't automatically restart when autoupdating.
 sed -i 's/ --apply//' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
