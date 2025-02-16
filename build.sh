56f4cfe77e09208509ed0e7334f5d71fb5873873
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-02-16 13:52:53 +0100

    feat: Add iommu kargs

--- build.sh
+++ build.sh
@@ -32,3 +32,12 @@ systemctl disable rpm-ostree-countme.timer
 
 # Don't automatically restart when autoupdating.
 sed -i 's/ --apply//' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
+
+cat >/usr/lib/bootc/kargs.d/00-vfio.toml <<EOF
+kargs = ["intel_iommu=on", "iommu=pt"]
+match-architectures = ["x86_64"]
+EOF
+
+KERNEL_SUFFIX=""
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v -f
